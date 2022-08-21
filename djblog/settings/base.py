a827b10f8f5d873be562dc9345891ab32aef2dc0
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-20 22:31:03 -0300

    cambida las rutas a static_root y staticfiles_dirs

--- djblog/settings/base.py
+++ djblog/settings/base.py
@@ -124,11 +124,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'),
-    os.path.join(os.path.dirname(BASE_DIR),"static"),
+    BASE_DIR / 'static',
 ]
 
 MEDIA_URL = 'media/'
