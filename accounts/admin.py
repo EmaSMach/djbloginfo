6d081986aba554642e327862bdd45963cc340f4a
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-24 10:51:19 -0300

    agregado algunos cambios en la información del usuario mostrado en el admin de django

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,5 +2,20 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'address', 'dni')
+    fieldsets = (
+        *UserAdmin.fieldsets,  # campos originales del usuario
+        (                      # Agregarmos nuestros campos extra
+            'Información adicional',  # header, puede ser None si no se quiere agregar un header
+            {
+                'fields': (
+                    'address',
+                    'dni',
+                    'role',
+                ),
+            },
+        ),
+    )
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, CustomUserAdmin)
