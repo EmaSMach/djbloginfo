587e0558758f8d93eef56bd67fddd2f776174085
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-25 23:07:11 -0300

    pasado los blog a la pagina de inicio del blog

--- home/views.py
+++ home/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from blog.models import Post
 # Create your views here.
 
 
 def index(request):
+    # traigo los últimos posts
+    posts = Post.objects.filter(active=True).order_by('-created_at')[:5]
     context = {
         'title': 'InfoBlog',
+        'posts': posts,
     }
     return render(request, 'home/index2.html', context=context)
