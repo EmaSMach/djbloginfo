781ca1562da8b62f53f729fff42546322a3d4f5e
David Emanuel Sandoval <davidemanuelsandoval@gmail.com>
2022-08-26 02:13:09 -0300

    agregado views_number al post

--- blog/models.py
+++ blog/models.py
@@ -20,6 +20,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     active = models.BooleanField(default=True)
+    views_number = models.IntegerField(default=0, null=True, blank=True)
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='posts')
     category = models.ManyToManyField(Category, related_name='posts', blank=True)
